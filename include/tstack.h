d777b7a9841f34cee2ee7ac5d67185ce9ec982a6
AntonKochubeev <160320922+AntonKochubeev@users.noreply.github.com>
2024-04-28 15:22:27 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,38 +1,36 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include "string"
 
 template<typename T, int size>
 class TStack {
+  // добавьте код стека
  private:
-    T arr[size];
-    int top;
+  T data[100];
+  int top;
 
-public:
-    TStack() : top(-1) {}
-
-    void push(T val) {
-        if (top < size - 1) {
-            arr[++top] = val;
-        }
+ public:
+  TStack() : top(-1) {}
+  void push(T value) {
+    if (!isFull())
+      data[++top] = value;
+    else
+      throw std::string("Full");
+  }
+  T show() const { return data[top]; }
+  T pop() {
+    if (isEmpty()) {
+      throw std::string("Empty");
+    } else {
+      return data[top--];
     }
-
-    T pop() {
-        if (top >= 0) {
-            return arr[top--];
-        }
-        return T();
-    }
-
-    T peek() {
-        if (top >= 0) {
-            return arr[top];
-        }
-        return T();
-    }
-
-    bool isEmpty() {
+  }
+    bool isEmpty() const {
         return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
     }
 };
 
